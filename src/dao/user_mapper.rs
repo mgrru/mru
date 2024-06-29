2c5dc9cad98063f0e4442b1d6ec24823b862fb78
mgrru <ru@fufu.moe>
2024-06-30 00:25:44 +0800

    test: 添加userMapper的测试

--- src/dao/user_mapper.rs
+++ src/dao/user_mapper.rs
@@ -5,9 +5,31 @@ use super::INIT_POOL;
 pub async fn get_all_users() -> Result<Vec<User>, sqlx::Error> {
     let pool = match INIT_POOL.get() {
         Some(p) => p,
-        None => return Err(sqlx::Error::Configuration("数据库未连接!".into())),
+        None => return Err(sqlx::Error::Configuration("连接池丢失!".into())),
     };
     sqlx::query_as::<_, User>("SELECT * FROM users")
         .fetch_all(pool)
         .await
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::dao::init_pool;
+
+    use super::*;
+    use dotenv::dotenv;
+
+    #[tokio::test]
+    async fn test_get_all_users() {
+        dotenv().ok();
+        let r = init_pool().await;
+        assert!(r.is_ok());
+
+        let users = match get_all_users().await {
+            Ok(us) => us,
+            Err(_) => vec![],
+        };
+
+        assert!(!users.is_empty());
+    }
 }
